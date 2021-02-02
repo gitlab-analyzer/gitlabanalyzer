b578c8eb9703a0e7cf14ce79871caad1dcefa1a3
HenryPC <fanghenry523@gmail.com>
2021-02-02 15:48:32 -0800

    Created gitlab_interface that can auth, getProjects and getCommits

--- server/gitlab_interface.py
+++ server/gitlab_interface.py
@@ -1,18 +1,55 @@
 import gitlab
 
-# class GitLab:
+
+class GitLab:
+    def __init__(self, token, url=None):
+        self.token = token
+        self.project_lists = None
+        if url is None:
+            self.url = "https://csil-git1.cs.surrey.sfu.ca/"
+        else:
+            self.url = url
+        self.gl = gitlab.Gitlab(self.url, self.token)
+
+    def authenticate(self):
+        try:
+            self.gl.auth()
+        except:
+            return False
+        return True
+
+    def get_project_list(self):
+        if not self.project_lists:
+            self.project_lists = self.gl.projects.list(visibility='private')
+        return self.project_lists
+
+    def get_commit_list_for_project(self, projectID):
+        myProjects = self.get_project_list()
+        for project in myProjects:
+            if project.id == projectID:
+                return project.commits.list()
+        return None
+
+    # Example: since='2016-01-01T00:00:00Z'
+    def get_commit_list_for_project_with_range(self, projectID, sinceDate, untilDate):
+        myProjects = self.get_project_list()
+        for project in myProjects:
+            if project.id == projectID:
+                return project.commits.list(since=sinceDate, until=untilDate)
+        return None
+
+    # Return a list of commit diff for a project
+    def get_commit_diff_list(self, projectID, sinceDate=None, untilDate=None):
+        commitDiff = []
+        if not (sinceDate and untilDate):
+            commitList = self.get_commit_list_for_project(projectID)
+        else:
+            commitList = self.get_commit_list_for_project_with_range(projectID, sinceDate, untilDate)
+
+        for oneCommit in commitList:
+            commitDiff.append(oneCommit.diff())
+        return commitDiff
 
 
-token = "uS4ai1UqTZJSeYp69Bey"
-gl = gitlab.Gitlab("https://csil-git1.cs.surrey.sfu.ca/", token)
-gl.auth()
 
-projects = gl.projects.list(visibility='private')
 
-# for project in projects:
-#     print(project)
-
-commits = projects[2].commits.list()
-
-for commit in commits:
-    print(commit)
