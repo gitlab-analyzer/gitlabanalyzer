5218c7794afcd61d2006bdc0b340254c9ce61bc2
Joshua Li Guo <jlguo@sfu.ca>
2021-02-03 21:04:30 -0800

    added Issue attribute stubs and getters

--- server/models/Issue.py
+++ server/models/Issue.py
@@ -1,5 +1,4 @@
-
-# GET /projects/:id/issues
+from typing import Union, Optional
 
 class Issue:
     """
@@ -29,5 +28,74 @@ class Issue:
         methods:
             Getters for all attributes
     """
+
     def __init__(self):
-        pass
+        self._issue_id: int = None
+        self._upvotes: int = None
+        self._downvotes: int = None
+        self._merge_requests_count: int = None
+
+        self._title: str = None
+        self._description: str = None
+        self._state: str = None
+        self._updated_date: str = None
+        self._created_date: str = None
+        self._closed_date: Optional[str] = None
+        self._due_date: Optional[str] = None
+        self._project_id: Union[int, str] = None
+
+        self._milestone = None
+        self._author = None
+
+        self._assignees_list = None
+        self._comments_list = None
+        self._labels_list = None
+
+    # Getters
+    @property
+    def issue_id(self) -> int:
+        return self._issue_id
+
+    @property
+    def upvotes(self) -> int:
+        return self._upvotes
+
+    @property
+    def downvotes(self) -> int:
+        return self._downvotes
+
+    @property
+    def merge_requests_count(self) -> int:
+        return self._merge_requests_count
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def description(self) -> str:
+        return self._description
+
+    @property
+    def state(self) -> str:
+        return self._state
+
+    @property
+    def updated_date(self) -> str:
+        return self._updated_date
+
+    @property
+    def created_date(self) -> str:
+        return self._created_date
+
+    @property
+    def closed_date(self) -> Optional[str]:
+        return self._closed_date
+
+    @property
+    def due_date(self) -> Optional[str]:
+        return self._due_date
+
+    @property
+    def project_id(self) -> Union[int, str]:
+        return self._project_id
