4e60582f3df06c21942f4a02cd56deb3bedf176a
Thomas Min <thomas_min@sfu.ca>
2021-02-08 20:49:26 -0800

    Fixed all the issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import Overview from './components/Overview';
 import Code from './components/Code';
@@ -6,13 +6,8 @@ import Table from './components/Table';
 import Batch from './components/Batch';
 import Config from './components/Config';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
-import { Container, Typography } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from '@material-ui/core';
 import './App.css';
 import './Shared.css';
 import LoginPage from './pages/LoginPage';
@@ -22,11 +17,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Header /> */}
         <Switch>
           <Route path="/" exact component={LoginPage} />
           <Route path="/repo" exact component={SearchPage} />
           <Container maxWidth="md">
+            <Header />
             <Route path="/overview" exact component={Overview} />
             <Route path="/code" component={Code} />
             <Route path="/table" component={Table} />
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,30 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 import { Paper, Button } from '@material-ui/core';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-// import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from '@material-ui/icons/Search';
 import useStyles from './BarStyles';
+import { useAuth } from '../context/AuthContext';
 
 const SearchBarComp = () => {
+  const [value, setValue] = useState('');
   const classes = useStyles();
+
+  const { user } = useAuth();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (user) {
+      console.log('Logged in');
+    } else {
+      console.log('Logged Out');
+    }
+  };
   return (
     <div className="main">
       <div className="bar_container">
-        <Paper component="form" className={classes.root}>
-          <IconButton className={classes.iconButton} aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <InputBase
-            className={classes.input}
-            placeholder="Search Repo"
-            inputProps={{ 'aria-label': 'search repo' }}
-          />
-          <Button variant="contained" className={classes.goButton}>
-            GO
+        <form className="flex" onSubmit={handleSubmit}>
+          <Paper className={classes.root}>
+            <IconButton className={classes.iconButton} aria-label="searchicon">
+              <SearchIcon />
+            </IconButton>
+            <InputBase
+              className={classes.input}
+              value={value}
+              onChange={(event) => {
+                setValue(event.target.value);
+              }}
+              placeholder="Search for Repo"
+              inputProps={{ 'aria-label': 'search repo' }}
+            />
+          </Paper>
+          <Button
+            type="submit"
+            variant="contained"
+            className={classes.goButton}
+          >
+            LOG IN
           </Button>
-        </Paper>
+        </form>
       </div>
     </div>
   );
