9e1cee6b922816d74d9c9e1ae7ac7be419ff574b
jiwonj <jiwonj@sfu.ca>
2021-02-12 14:54:57 -0800

    Add more variables (iid, corresponding issue iid)

--- server/models/MergeRequest.py
+++ server/models/MergeRequest.py
@@ -2,8 +2,9 @@ from typing import Union, Optional, List
 
 class MergeRequest:
 
-	def __init__(self, id, author, title, description, state, merged_by=None, created_date, merged_date=None, comments=None) -> None:
-		self.__id: Union[int, str] = id 
+	def __init__(self, id, iid, author, title, description, state, merged_by=None, created_date, merged_date=None, related_issue_iid, comments=None) -> None:
+		self.__id: int = id
+		self.__iid: int = iid
 		self.__author: str = author 
 		self.__title: str = title
 		self.__description: str = description 
@@ -11,13 +12,18 @@ class MergeRequest:
 		self.__merged_by: Optional[str] = merged_by 
 		self.__created_date: str = created_date  #datetime in ISO 8601 format
 		self.__merged_date: Optional[str] = merged_date
+		self.__related_issue_iid: int = related_issue_iid #Ex. 23 for a merge request closing Issue #23
 		self.__comments: Optional[List[str]] = comments 
 
 	#Getters
 
 	@property
-	def id(self) -> Union[int, str]:
+	def id(self) -> int:
 		return self.__id
+
+	@property
+	def iid(self) -> int:
+	    return self.__iid
 	
 	@property
 	def author(self) -> str:
@@ -46,6 +52,10 @@ class MergeRequest:
 	@property
 	def merged_date(self) -> Optional[str]:
 		return self.__merged_date
+
+	@property
+	def related_issue_iid(self) -> int:
+    		return self.__related_issue_iid
 	
 	@property
 	def comments(self) -> Optional[List[str]]:
