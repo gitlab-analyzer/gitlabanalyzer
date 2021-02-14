eb5fb5fd29e038f8c34b5f81f363c541be735884
jiwonj <jiwonj@sfu.ca>
2021-02-14 13:22:09 -0800

    Add to_json and str methods, Fix minor errors

--- server/models/Comment.py
+++ server/models/Comment.py
@@ -3,7 +3,7 @@ from typing import Union, Optional
 """
 For comments from Issue / Merge Request:
 * id => id of the comment itself. None if comment is on "Commit"
-* noteable_type => Type of the note where comment is written (Ex. "Issue", "MergeRequest"). 
+* noteable_type => Type of the note where comment is written (Ex. "Issue", "MergeRequest").
 * noteable_id => id/sha of the note. (Ex. Issue id #28330, Commit sha 8640481e)
 * noteable_iid => iid of the note. (Ex. Issue iid #1) None if comment is on "Commit"
 
@@ -16,14 +16,20 @@ For comments from Commit:
 
 class Comment:
 
-	def __init__(self, id=None, author, body, created_date, noteable_id, noteable_type, noteable_iid=None) -> None:
-		self.__id: Optional[int] = id 	#id of the comment itself
-		self.__author: str = author #Member?
-		self.__body: str = body	
+	def __init__(self, author, body, created_date, noteable_id, noteable_type, noteable_iid=None, id=None) -> None:
+		self.__author: int = author #Author id
+		self.__body: str = body
 		self.__created_date: str = created_date  #datetime in ISO 8601 format
 		self.__noteable_id: Union[int, str] = noteable_id  	#Ex. Issue of id #28830
-		self.__noteable_type: str = comment_type  #whether comment is on Issue / MergeRequest / Commit 
+		self.__noteable_type: str = noteable_type  #whether comment is on Issue / MergeRequest / Commit
 		self.__noteable_iid: Optional[Union[int, str]] = noteable_iid # Ex. Issue #1
+		self.__id: Optional[int] = id 	#id of the comment itself
+
+	def to_json(self) -> str:
+		return self.__dict__.__str__().replace("_Comment__", "").replace("'", "\"")
+
+	def __str__(self) -> str:
+		return self.__dict__.__str__()
 
 	#Getters
 
@@ -32,21 +38,21 @@ class Comment:
 		return self.__id
 
 	@property
-	def author(self) -> str:
+	def author(self) -> int:
 		return self.__author
-	
+
 	@property
 	def body(self) -> str:
 		return self.__body
-	
+
 	@property
 	def created_date(self) -> str:
 		return self.__created_date
-	
+
 	@property
 	def noteable_id(self) -> Union[int, str]:
 		return self.__noteable_id
-	
+
 	@property
 	def noteable_type(self) -> str:
 		return self.__noteable_type
@@ -54,6 +60,15 @@ class Comment:
 	@property
 	def noteable_iid(self) -> Optional[Union[int, str]]:
 		return self.__noteable_iid
-	
-	
-	
+
+
+"""
+#Test
+
+test = Comment(123, "body", "CREATED date", 28830, "Issue", noteable_iid=1, id=12345)
+
+print(test)
+print("_______")
+print(test.to_json())
+
+"""
