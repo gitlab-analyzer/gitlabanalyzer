110cdfb10e2048ad53c0c490d9cc4323ac75d9c9
Henry Fang <fanghaof@sfu.ca>
2021-02-15 17:15:16 -0800

    Finished merge_request_manager

--- server/manager/merge_request_manager.py
+++ server/manager/merge_request_manager.py
@@ -1,26 +1,36 @@
+from typing import Union
+import datetime
+from dateutil import parser
 from interface.gitlab_interface import GitLab
-from model import *
+from model.MergeRequest import MergeRequest
 import gitlab
 
 
 class MergeRequestManager:
     def __init__(self) -> None:
-        self.mergeRequestList: list = []
+        self.__mergeRequestList: list = []
 
-    def add_merge_request(self, mergeRequest: gitlab):
-        pass
+    def add_merge_request(self, mergeRequest: gitlab) -> None:
+        self.__mergeRequestList.append(MergeRequest(mergeRequest))
 
+    def get_merge_request_by_id(self, myId) -> Union[MergeRequest, None]:
+        for mergeRequest in self.__mergeRequestList:
+            if mergeRequest.id == myId:
+                return mergeRequest
+        return None
 
+    def get_merge_request_by_iid(self, myIid) -> Union[MergeRequest, None]:
+        for mergeRequest in self.__mergeRequestList:
+            if mergeRequest.iid == myIid:
+                return mergeRequest
+        return None
 
-gl = GitLab("bksg7zzFjaShH5s1mbRi", "https://csil-git1.cs.surrey.sfu.ca/")
-gl.authenticate()
-pList = gl.get_project_list()
-# for p in pList:
-#     print(p)
-gl.set_project(25515)
-mr, commits = gl.get_merge_requests_and_commits()
-count = 0
-for _ in mr:
-    print(mr[count])
-    print(commits[count][0])
-    count = count + 1
+    def get_merge_requests_by_range(self, startDate, endDate) -> list:
+        myStartDate: datetime = parser.parse(startDate)
+        myEndDate: datetime = parser.parse(endDate)
+        tempMergeRequestList: list = []
+        for mergeRequest in self.__mergeRequestList:
+            tempStartDate = mergeRequest.created_date
+            if myStartDate <= tempStartDate <= myEndDate:
+                tempMergeRequestList.append(mergeRequest)
+        return tempMergeRequestList
