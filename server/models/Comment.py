13d874aa4ec3d0bd6eff4a649a40a94d617c9a3b
jiwonj <jiwonj@sfu.ca>
2021-02-17 05:47:23 -0800

    Modify data class to use gitlab

--- server/models/Comment.py
+++ server/models/Comment.py
@@ -1,4 +1,5 @@
 from typing import Union, Optional
+import gitlab
 
 """
 For comments from Issue / Merge Request:
@@ -16,14 +17,23 @@ For comments from Commit:
 
 class Comment:
 
-	def __init__(self, author, body, created_date, noteable_id, noteable_type, noteable_iid=None, id=None) -> None:
-		self.__author: int = author #Author id
-		self.__body: str = body
-		self.__created_date: str = created_date  #datetime in ISO 8601 format
-		self.__noteable_id: Union[int, str] = noteable_id  	#Ex. Issue of id #28830
-		self.__noteable_type: str = noteable_type  #whether comment is on Issue / MergeRequest / Commit
-		self.__noteable_iid: Optional[Union[int, str]] = noteable_iid # Ex. Issue #1
-		self.__id: Optional[int] = id 	#id of the comment itself
+	def __init__(self, commentForIssueMR: gitlab = None, commentForCommit: gitlab = None, commitSha: str = None) -> None:
+		if commentForIssueMR is not None: 	#comment of either MergeRequest or Issue
+			self.__author: int = commentForIssueMR.author['id']
+			self.__body: str = commentForIssueMR.body
+			self.__created_date: str = commentForIssueMR.created_at
+			self.__noteable_id: Union[int, str] = commentForIssueMR.noteable_id
+			self.__noteable_type: str = commentForIssueMR.noteable_type  #whether comment is on Issue / MergeRequest / Commit
+			self.__noteable_iid: Optional[int] = commentForIssueMR.noteable_iid # Ex. Issue #1
+			self.__id: Optional[int] = commentForIssueMR.id
+		else: 		#comment of Commit
+			self.__author: int = commentForCommit.author['id']
+			self.__body: str = commentForCommit.note
+			self.__created_date: str = commentForCommit.created_at  #datetime in ISO 8601 format
+			self.__noteable_id: Union[int, str] = commitSha
+			self.__noteable_type: str = "Commit"
+			self.__noteable_iid: Optional[Union[int, str]] = None
+			self.__id: Optional[int] = None
 
 	def to_json(self) -> str:
 		return self.__dict__.__str__().replace("_Comment__", "").replace("'", "\"")
