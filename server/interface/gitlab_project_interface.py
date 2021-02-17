ff4af764af14f607be4e7ad7e8c51020657a494f
Henry Fang <fanghaof@sfu.ca>
2021-02-17 15:05:53 -0800

    initialize GitlabProject interface code skeleton

---
+++ server/interface/gitlab_project_interface.py
@@ -0,0 +1,13 @@
+from model import *
+import gitlab_interface
+
+
+class GitlabProject:
+    def __init__(self):
+        # self.__membersManager
+        # self.__issuesManager
+        # self.__commitsManager
+        # self.__commentsManager
+        # self.__mergeRequestManager
+        pass
+
