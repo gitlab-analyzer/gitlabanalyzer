844a0506cfb64a7084e98a07e2f611902fc9d084
jaddiet <jaddie_tan@sfu.ca>
2021-02-17 03:15:30 -0800

    add mousewheel scroll

--- client/src/components/FloatBar.js
+++ client/src/components/FloatBar.js
@@ -5,55 +5,24 @@ import {Button, Select, StylesProvider /*createMuiTheme*/} from '@material-ui/co
 import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
 import FormControl from '@material-ui/core/FormControl';
 
-// import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import { /*makeStyles, ThemeProvider*/ } from '@material-ui/core/styles';
+// import { /*makeStyles, ThemeProvider*/ } from '@material-ui/core/styles';
 // import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Drawer from '@material-ui/core/Drawer';
+// import ReactDOM from 'react-dom'
+import HorizontalScroll from './Scroll'
 
 const SCORES = ['raw score', 'commits', 'lines of code', "issues & reviews"];
-const USERS = ['bfraser', 'khangura', 'gbaker', 'prof1'];
+const USERS = ['bfraser', 'khangura', 'gbaker', 'afraser'];
 const DATES = ['8/29/2021', '9/31/2021'];
 
-// const useStyles = makeStyles((theme) => ({
-//   listofusers: {    
-//     backgroundColor: "#4A4F68",
-    
-//     height: "50px",
-//     borderRadius: "10px",
-//     MuiOutlinedInput: {
-//       outline:"none",
-//     },
-
-//   },
-//   overrides: {
-//     MuiInput: {
-//       // Name of the styleSheet
-//       listofusers: {
-//         '&:hover:not($disabled):before': {
-//           // backgroundColor: theme.palette.input.MuiInput,
-//           backgroundColor: theme.palette.text.primary,
-//           // height: 1,
-//           outline:"none",
-//         },
-//         '&:after': {
-//           borderBottom: `2px solid red`
-//         },
-//       },
-//     },
-//   },
-// }));
 
 function FloatBar() {
 
-  // const classes = useStyles();
   const [user, setUser] = React.useState('');
+  const scrollRef = HorizontalScroll();
 
-  // const handleChange = (event) => {
-  //   setUser(event.target.value);
-  // };
+
   const handleUserChange = (event, index) => {
     setUser(event.target.value);
   };
@@ -66,56 +35,58 @@ function FloatBar() {
             })}
 
           </ul>
-          <div className="floatbar-scores">
+          <div className="floatbar-scores" ref={scrollRef}>
+              <div classname="userscore">
+                {SCORES.map(scorename => {
+                  return <li>{scorename}</li>;
+                })}
+              </div>
+              <div classname="userscore">
+                {SCORES.map(scorename => {
+                  return <li>{scorename}</li>;
+                })}
+              </div>
+              <div classname="userscore">
+                {SCORES.map(scorename => {
+                  return <li>{scorename}</li>;
+                })}
+              </div>
+              <div classname="userscore">
+                {SCORES.map(scorename => {
+                  return <li>{scorename}</li>;
+                })}
+              </div>
+              <div classname="userscore">
+                {SCORES.map(scorename => {
+                  return <li>{scorename}</li>;
+                })}
+              </div>
+              <div classname="userscore">
+                {SCORES.map(scorename => {
+                  return <li>{scorename}</li>;
+                })}
+              </div>
+              <div classname="userscore">
+                {SCORES.map(scorename => {
+                  return <li>{scorename}</li>;
+                })}
+              </div>
+              <div classname="userscore">
+                {SCORES.map(scorename => {
+                  return <li>{scorename}</li>;
+                })}
+              </div>
+              <div classname="userscore">
+                {SCORES.map(scorename => {
+                  return <li>{scorename}</li>;
+                })}
+              </div>
+              <div classname="userscore">
+                {SCORES.map(scorename => {
+                  return <li>{scorename}</li>;
+                })}
+              </div>
 
-              <div>
-                {SCORES.map(scorename => {
-                  return <li>{scorename}</li>;
-                })}
-              </div>
-              <div>
-                {SCORES.map(scorename => {
-                  return <li>{scorename}</li>;
-                })}
-              </div>
-              <div>
-                {SCORES.map(scorename => {
-                  return <li>{scorename}</li>;
-                })}
-              </div>
-              <div>
-                {SCORES.map(scorename => {
-                  return <li>{scorename}</li>;
-                })}
-              </div>
-              <div>
-                {SCORES.map(scorename => {
-                  return <li>{scorename}</li>;
-                })}
-              </div>
-              <div>
-                {SCORES.map(scorename => {
-                  return <li>{scorename}</li>;
-                })}
-              </div>
-              <div>
-                {SCORES.map(scorename => {
-                  return <li>{scorename}</li>;
-                })}
-              </div>
-              <div>
-                {SCORES.map(scorename => {
-                  return <li>{scorename}</li>;
-                })}
-              </div>
-              <div>
-                {SCORES.map(scorename => {
-                  return <li>{scorename}</li>;
-                })}
-              </div>
-             
-  
-            {/* </Drawer> */}
 
          
 
@@ -186,5 +157,7 @@ function FloatBar() {
     );
   
 }
+
+
 
 export default FloatBar;
