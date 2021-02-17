f6ab9727b072b271df23d1ada3b0ab38297365ab
springbro294 <66364910+springbro294@users.noreply.github.com>
2021-02-18 03:21:09 +0700

    format the code + add manager

--- server/models/Member.py
+++ server/models/Member.py
@@ -1,33 +1,39 @@
+import gitlab
 from typing import Union, Optional, List
-class MergeRequest:
-	def __init__(self, id, username, name, state, access_level) -> None:
-		self.__id: Union[int, str] = id 
-		self.__username: str = username
-		self.__name: str = name
-		self.__state: str = state
-		self.__access_level: int = access_level
 
-	#Getters
-	
-	@property
-	def id(self) -> Union[int, str]:
-		return self.__id
-	
-	@property
-	def username(self) -> str:
-		return self.__username
-	
-	@property
-	def name(self) -> str:
-		return self.__name
-	
-	@property
-	def state(self) -> str:
-		return self.__state
 
-	@property
-	def access_level(self) -> int:
-		return self.__access_level
-	
-	
-	
+class Member:
+    def __init__(self, member: gitlab) -> None:
+        self.__id: int = member.id
+        self.__username: str = member.username
+        self.__name: str = member.name
+        self.__state: str = member.state
+        self.__access_level: int = member.access_level
+
+    def to_json(self) -> str:
+        return self.__dict__.__str__().replace("_Member__", "").replace("'", '"')
+
+    def __str__(self) -> str:
+        return self.__dict__.__str__()
+
+    # Getters
+
+    @property
+    def id(self) -> Union[int, str]:
+        return self.__id
+
+    @property
+    def username(self) -> str:
+        return self.__username
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def state(self) -> str:
+        return self.__state
+
+    @property
+    def access_level(self) -> int:
+        return self.__access_level
