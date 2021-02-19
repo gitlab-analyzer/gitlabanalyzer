ccbda9c4f05b988197ee58fb7a97ce950045a913
Thomas Min <thomas_min@sfu.ca>
2021-02-18 20:11:16 -0800

    Fixed package json issue

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import {Navbar, Nav} from 'react-bootstrap'
-import {LinkContainer} from 'react-router-bootstrap'
-import Logo from './Logo'
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import Logo from './Logo';
 
 const Header = () => {
   return (
     <div>
       <Logo />
-    <Navbar bg="light" expand="lg">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-        <LinkContainer to="/overview">
-        <Nav.Link>Overview</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/code">
-        <Nav.Link>Commits & Code</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/table">
-        <Nav.Link>Issues & Reviews</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/batch">
-        <Nav.Link>Batch Processing</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/config">
-        <Nav.Link>Config</Nav.Link>
-        </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer to="/overview">
+              <Nav.Link>Overview</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/commits">
+              <Nav.Link>Commits & Code</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/table">
+              <Nav.Link>Issues & Reviews</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/batch">
+              <Nav.Link>Batch Processing</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/config">
+              <Nav.Link>Config</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
