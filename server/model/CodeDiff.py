32be041001d3a17ee8b8191e1667869e4d359e5f
Henry Fang <fanghaof@sfu.ca>
2021-02-20 17:06:05 -0800

    added codeDiff dataclass

---
+++ server/model/CodeDiff.py
@@ -0,0 +1,47 @@
+import gitlab
+
+
+class CodeDiff:
+    def __init__(self, codeDiffObject: gitlab):
+        self.__new_file: bool = codeDiffObject.new_file
+        self.__renamed_file: bool = codeDiffObject.renamed_file
+        self.__deleted_file: bool = codeDiffObject.deleted_file
+        self.__diff: str = codeDiffObject.diff
+        self.__old_path: str = codeDiffObject.old_path
+        self.__new_path: str = codeDiffObject.new_path
+        self.__a_mode: str = codeDiffObject.a_mode
+        self.__b_mode: str = codeDiffObject.b_mode
+
+        super().__init__()
+
+    @property
+    def new_file(self) -> bool:
+        return self.__new_file
+
+    @property
+    def renamed_file(self) -> bool:
+        return self.__renamed_file
+
+    @property
+    def deleted_file(self) -> bool:
+        return self.__deleted_file
+
+    @property
+    def diff(self) -> str:
+        return self.__diff
+
+    @property
+    def old_path(self) -> str:
+        return self.__old_path
+
+    @property
+    def new_path(self) -> str:
+        return self.__new_path
+
+    @property
+    def a_mode(self) -> str:
+        return self.__a_mode
+
+    @property
+    def b_mode(self) -> str:
+        return self.__b_mode
