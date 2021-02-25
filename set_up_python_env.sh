ed0c86e33f32575e8d7ac00b634a6a23446737a5
Henry Fang <fanghaof@sfu.ca>
2021-02-24 22:43:39 -0800

    Fixed uncleaned merge conflict in server/.gitignore and added script to setup python env

---
+++ set_up_python_env.sh
@@ -0,0 +1,11 @@
+sudo apt update
+echo installling python3
+sudo apt install python3
+echo installling pip for python3
+sudo apt install python3-pip
+echo installling required python packages
+sudo pip3 install flask
+sudo pip3 install -U flask-cors
+sudo pip3 install pymongo
+sudo pip3 install python-dateutil
+sudo pip3 install python-gitlab
