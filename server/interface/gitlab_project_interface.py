8615a5d298675e4289191bb9816255d1710eac78
Guojiaxi <joshliguo@gmail.com>
2021-03-08 14:18:50 -0800

    added asserts for some error checking

--- server/interface/gitlab_project_interface.py
+++ server/interface/gitlab_project_interface.py
@@ -105,6 +105,7 @@ class GitLabProject:
                 commitScoreData = self.get_commit_score_data(commit.id)
 
                 for key1, key2 in zip(scoreData.keys(), commitScoreData.keys()):
+                    assert key1 == key2
                     scoreData[key1] += commitScoreData[key2]
         
         return scoreData
@@ -123,6 +124,7 @@ class GitLabProject:
             mergeRequestScoreData = self.get_merge_request_score_data(mr.id)
 
             for key1, key2 in zip(scoreData.keys(), mergeRequestScoreData.keys()):
+                assert key1 == key2
                 scoreData[key1] += scoreData[key2]
         
         return scoreData
