d10c357f482cc2da4d068b16dea77410e1f876d6
xtran <xuan_anh_tran@sfu.ca>
2021-03-11 23:49:49 +0700

    fix edge case and add return to support API calls

--- server/manager/code_diff_temp.py
+++ server/manager/code_diff_temp.py
@@ -11,85 +11,143 @@ class codeDiffManager:
     def get_code_diff_list(self) -> list:
         return self.__codeDiffList
 
-    def get_code_diff_statistic(self, codeDiffObject: gitlab) -> None:
+    def get_code_diff_statistic(self, codeDiffObject: gitlab) -> dict:
         newLine = 0
         deleteLine = 0
+        newCommentLine = 0
+        deleteCommentLine = 0
         syntax = 0
+        spacing = 0
         oldLine = ""
-        isComment = False
-        isSpacing = True
         python = False
 
         self.check_for_code_type(codeDiffObject)
 
         diffCode = CodeDiff(codeDiffObject)
         for line in diffCode.diff.splitlines():
-            if oldLine in line:
-                self.modify_to_a_line(
-                    newLine, syntax, isComment, isSpacing, oldLine, line, python
+
+            if oldLine[1:] in line[1:] and oldLine[0] != line[0]:
+                tempLine = '+' + line[1:].replace(oldLine[1:], '')
+                self.modify_to_a_new_line(
+                    newLine,
+                    deleteLine,
+                    newCommentLine,
+                    deleteCommentLine,
+                    spacing,
+                    syntax,
+                    tempLine,
+                    python,
+                )
+            if line[1:] in oldLine[1:] and oldLine[0] != line[0]:
+                tempLine = '-' + oldLine[1:].replace(line[1:], '')
+                self.modify_to_a_new_line(
+                    newLine,
+                    deleteLine,
+                    newCommentLine,
+                    deleteCommentLine,
+                    spacing,
+                    syntax,
+                    tempLine,
+                    python,
                 )
             else:
                 self.modify_to_a_new_line(
-                    newLine, deleteLine, syntax, isComment, isSpacing, line, python
+                    newLine,
+                    deleteLine,
+                    newCommentLine,
+                    deleteCommentLine,
+                    spacing,
+                    syntax,
+                    line,
+                    python,
                 )
 
-    def modify_to_a_line(
-        self, newLine, syntax, isComment, isSpacing, oldLine, line, python
-    ) -> None:
-        diffs = set(char for char in line[1:]) - set(char for char in oldLine[1:])
-        if len(diffs) == 1:
-            if python is False:
-                if "{" == diffs or "}" == diffs:
-                    syntax = syntax + 1
-            elif python is True:
-                if ":" == diffs:
-                    syntax = syntax + 1
-            else:
-                newLine = newLine + 1
-        elif oldLine in line:
-            tempLine = line.replace(oldLine, '')
-            self.check_for_spacing_or_comment(isComment, isSpacing, tempLine, python)
-            if not isComment or not isSpacing:
-                newLine = newLine + 1
+        info = {
+            "lines_added": newLine,
+            "lines_deleted": deleteLine,
+            "comments_added": newCommentLine,
+            "comments_deleted": deleteCommentLine,
+            "spacing_changes": spacing,
+            "syntax_changes": syntax,
+        }
+        return info
 
     def modify_to_a_new_line(
-        self, newLine, deleteLine, syntax, isComment, isSpacing, line, python
+        self,
+        newLine,
+        deleteLine,
+        newCommentLine,
+        deleteCommentLine,
+        spacing,
+        syntax,
+        line,
+        python,
     ) -> None:
-        self.check_for_spacing_or_comment(isComment, isSpacing, line[1:], python)
+
+        if len(line[1:]) == 0:
+            spacing = spacing + 1
+            return
+
+        if self.check_for_spacing_or_comment(
+            line[0:1], newCommentLine, deleteCommentLine, spacing, line[1:], python
+        ):
+            return
+
         if line[0:1] == '+':
             newLine = newLine + 1
             if python is False:
-                if "{" == line or "}" == line:
+                if "{" == line[1:] or "}" == line[1:]:
                     syntax = syntax + 1
                     newLine = newLine - 1
             if python is True:
-                if ":" == line:
+                if ":" == line[1:]:
                     syntax = syntax + 1
                     newLine = newLine - 1
-            if line == '+' or isComment or isSpacing:
-                newLine = newLine - 1
+
         if line[0:1] == '-':
             deleteLine = deleteLine + 1
-            if line == '-':
-                deleteLine = deleteLine - 1
+            if python is False:
+                if "{" == line[1:] or "}" == line[1:]:
+                    syntax = syntax + 1
+                    deleteLine = deleteLine - 1
+            if python is True:
+                if ":" == line[1:]:
+                    syntax = syntax + 1
+                    deleteLine = deleteLine - 1
 
-    def check_for_spacing_or_comment(self, isComment, isSpacing, str, python) -> None:
+    def check_for_spacing_or_comment(
+        self, signal, newCommentLine, deleteCommentLine, spacing, str, python
+    ) -> bool:
+        isSpacing = False
+        isModify = False
+
         for i in str:
             if i == " ":
-                isComment = False
                 isSpacing = True
             elif i == "#" and python is True:
-                isComment = True
+                isModify = True
+                if signal == '+':
+                    newCommentLine = newCommentLine + 1
+                else:
+                    deleteCommentLine = deleteCommentLine + 1
                 isSpacing = False
                 break
             elif i == "//" and python is False:
-                isComment = True
+                isModify = True
+                if signal == '+':
+                    newCommentLine = newCommentLine + 1
+                else:
+                    deleteCommentLine = deleteCommentLine + 1
                 isSpacing = False
                 break
             else:
                 isSpacing = False
-                isComment = False
                 break
+        if isSpacing:
+            spacing = spacing + 1
+            isModify = True
+
+        return isModify
 
     def check_for_code_type(self, codeDiffObject: gitlab) -> None:
         diffCode = CodeDiff(codeDiffObject)
