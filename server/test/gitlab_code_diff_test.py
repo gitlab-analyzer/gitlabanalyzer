79c02a7c8a8bbaaa8399656ea3c6dd140c2eedc4
Henry Fang <fanghaof@sfu.ca>
2021-03-11 23:35:21 -0800

    Added test file for code_diff_analyzer

---
+++ server/test/gitlab_code_diff_test.py
@@ -0,0 +1,14 @@
+from interface.gitlab_interface import GitLab
+import gitlab
+
+from interface.gitlab_project_interface import GitLabProject
+
+"""
+Below are only for testing purpose
+"""
+if __name__ == "__main__":
+    myGitLab = GitLab(token="EJf7qdRqxdKWu1ydozLe", url="https://cmpt373-1211-12.cmpt.sfu.ca/")
+    if myGitLab.authenticate():
+        print("authenticated")
+    print("Updating all managers...")
+    gitlabProjectInterface = GitLabProject(myGitLab, 2)
