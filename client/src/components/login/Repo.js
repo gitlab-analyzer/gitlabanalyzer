175887bb829ad5d83d411469e34da25bfe6162e0
Thomas Min <thomas_min@sfu.ca>
2021-03-13 14:44:53 -0800

    Change export PORT, uncomment setproject

--- client/src/components/login/Repo.js
+++ client/src/components/login/Repo.js
@@ -256,7 +256,7 @@ const Repo = ({
        * This call is disabled for the demo on Monday.
        */
 
-      // await setProjectId();
+      await setProjectId();
 
       await fetchMembers();
       await fetchUsers();
