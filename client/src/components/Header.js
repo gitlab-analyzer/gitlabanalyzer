0de480bb6cc3f7a88328c7701bc915799ea242f1
Thomas Min <thomas_min@sfu.ca>
2021-03-13 11:06:04 -0800

    Add margin to the navigation menu items

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import { Navbar, Nav, Link } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import FloatBar from './floatbar/FloatBar';
 import Logo from './Logo';
+import './Header.css';
 
 const Header = () => {
   return (
@@ -13,19 +14,19 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <LinkContainer to="/summary">
+            <LinkContainer className="marginRight" to="/summary">
               <Nav.Link>Summary</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/commits">
+            <LinkContainer className="marginRight" to="/commits">
               <Nav.Link>Commits & MRs</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/table">
+            <LinkContainer className="marginRight" to="/table">
               <Nav.Link>Issues & Reviews</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/batch">
+            <LinkContainer className="marginRight" to="/batch">
               <Nav.Link>Batch Processing</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/config">
+            <LinkContainer className="marginRight" to="/config">
               <Nav.Link>Config</Nav.Link>
             </LinkContainer>
           </Nav>
