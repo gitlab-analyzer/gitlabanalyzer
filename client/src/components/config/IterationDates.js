6b5a7b0f3b469e0f8f42a1de4df61f3ea8ff8c48
jaddiet <jaddie_tan@sfu.ca>
2021-03-13 23:38:49 -0800

    Seperate in Multiple Files

---
+++ client/src/components/config/IterationDates.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { DatePicker, Form } from 'antd';
+import { setting } from '../../pages/InitialConfig';
+
+const { RangePicker } = DatePicker;
+
+function IterationDates() {
+    const DateErrorMsg = 'Please input a Date Range.'
+    return (
+        <div>
+            <Form.Item
+                label="Iteration 1"
+                name="iter1"
+                rules={[
+                    {
+                        required: true,
+                        message: DateErrorMsg,
+                    },
+                ]}
+            >
+                <RangePicker 
+                format="YYYY/MM/DD hh:mm:ss"
+                showTime
+                onChange={value => 
+                    {
+                        setting.iteration.iter1start = value[0].format();
+                        setting.iteration.iter1end = value[1].format();
+                    }
+                }
+                />
+            </Form.Item>
+            <Form.Item
+                label="Iteration 2"
+                name="iter2"
+                rules={[
+                    {
+                        required: true,
+                        message: DateErrorMsg,
+                    },
+                ]}
+            >
+                <RangePicker 
+                format="YYYY/MM/DD hh:mm:ss"
+                showTime
+                onChange={value => 
+                    {
+                        setting.iteration.iter2start = value[0].format();
+                        setting.iteration.iter2end = value[1].format();
+                    }
+                }
+                />
+            </Form.Item>
+            <Form.Item
+                label="Iteration 3"
+                name="iter3"
+                rules={[
+                    {
+                        required: true,
+                        message: DateErrorMsg,
+                    },
+                ]}
+            >
+                <RangePicker 
+                format="YYYY/MM/DD hh:mm:ss"
+                showTime
+                onChange={value => 
+                    {
+                        setting.iteration.iter3start = value[0].format();
+                        setting.iteration.iter3end = value[1].format();
+                    }
+                }
+                />
+            </Form.Item>
+        </div>
+    );
+}
+
+export default IterationDates;
