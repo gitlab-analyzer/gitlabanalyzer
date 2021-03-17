badc9a68f4e223209fb7b9c840762595ba81b7f2
jaddiet <jaddie_tan@sfu.ca>
2021-03-16 20:31:06 -0700

    change individual scores smaller

--- client/src/components/SelectUser.js
+++ client/src/components/SelectUser.js
@@ -9,10 +9,20 @@ const { Option } = Select;
 function SelectUser() {
   const { selectMembersList, selectUser, setSelectUser } = useAuth();
   return (
-    <div className="selectUser bgwhite">
+    <div 
+      className="selectUser bgwhite" 
+      style={{
+        display:'flex', 
+        justifyContent:'space-between',
+        paddingBottom:'10px'
+      }}
+    >
       <Select
         defaultValue={selectUser}
-        style={{ width: 200 }}
+        style={{ 
+          width: 200, 
+          marginRight:'10px'
+        }}
         onChange={(value) => {
           setSelectUser(value);
         }}
