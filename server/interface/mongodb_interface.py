ebe413b402475e98649769a29754b49f21ccfc89
Joshua Li Guo <jlguo@sfu.ca>
2021-03-20 15:39:41 -0700

    added mongodb class file

---
+++ server/interface/mongodb_interface.py
@@ -0,0 +1,16 @@
+import pymongo
+
+class MongoDB:
+    # Below is just an example to use mangodb
+    # username = urllib.parse.quote_plus('root')
+    # password = urllib.parse.quote_plus('pass')
+    # myClient = pymongo.MongoClient(
+    #     "mongodb://%s:%s@mangodb:27017/" % (username, password))
+    # myDB = myClient["student_repo"]
+    # myCol = myDB["students"]
+    #
+    # myCol.insert_one({"name": "John", "repoInfo": "this is a test"})
+    #
+    # print(myDB.list_collection_names(), flush=True)
+    # print(myCol.find_one(), flush=True)
+    pass
