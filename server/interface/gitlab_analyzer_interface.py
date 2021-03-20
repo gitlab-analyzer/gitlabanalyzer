cdbc81c231c019e162a02e961dddeba73390f266
jiwonj <jiwonj@sfu.ca>
2021-03-20 15:39:54 -0700

    add function to find project by id

--- server/interface/gitlab_analyzer_interface.py
+++ server/interface/gitlab_analyzer_interface.py
@@ -1,51 +1,44 @@
 from interface.gitlab_project_interface import GitLabProject
 from interface.gitlab_interface import GitLab
-from typing import List
+from typing import List, Optional
 
 
 class GitLabAnalyzer:
-    def __init__(self):
-        self.__user_list: List[str] = []
-
-        # list of repositories that user has access
+    def __init__(self, user_token=None, username=None):
         self.__project_list: List[GitLabProject] = []
+        self.__user_token: str = user_token  # hashed
+        self.__username: str = username  # related to user_token
 
-        # hashed tokens
-        self.__user_token_list: List[str] = []
-        self.__current_user_token: str = None
-
-        self.__url: str = None  # not sure if needed
-
-        self.__analyzing_repository: GitLabProject = None
-        self.__analyzing_user: str = None  # username
-
-    def add_project(self, project: GitLabProject):
+    def add_project(self, project: GitLabProject) -> None:
         self.__project_list.append(project)
 
-    def add_user(self, user):
-        self.__user_list.append(user)
-
-    def add_token(self, token):
-        self.__user_token_list.append(token)
-
-    def update_project_list(self, gitlab: GitLab):
+    def update_project_list(self, gitlab: GitLab) -> None:
         projectList = gitlab.get_project_list()
 
         for project in projectList:
             gitlabProjectInterface = GitLabProject(gitlab, project.id)
             self.__project_list.append(gitlabProjectInterface)
 
-    def set_current_user_token(self, hashed_str):
-        self.__current_user_token = hashed_str
+    def set_user_token(self, hashed_token) -> None:
+        self.__current_user_token = hashed_token
 
-    @property
-    def user_list(self):
-        return self.__user_list
+    def set_username(self, user_name) -> None:
+        self.__username = user_name
+
+    def get_gitlab_proejct_by_id(self, projectID) -> Optional[GitLabProject]:
+        for project in self.__project_list:
+            if project.project_id == projectID:
+                return project
+        return None
 
     @property
     def project_list(self):
         return self.__project_list
 
     @property
-    def current_user_token(self):
-        return self.__current_user_token
+    def user_token(self):
+        return self.__user_token
+
+    @property
+    def username(self):
+        return self.__username
