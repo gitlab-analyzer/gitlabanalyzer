cb4865222ef0d17010285dfbc2ac437f0a554cb8
Thomas Min <thomas_min@sfu.ca>
2021-03-23 23:13:45 -0700

    Add files changed component

--- client/src/pages/CodeDiff.js
+++ client/src/pages/CodeDiff.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { parseDiff, Diff, Hunk, Decoration } from 'react-diff-view';
-import { Checkbox } from 'antd';
+import { Checkbox, List, Button } from 'antd';
+import { FileExcelOutlined } from '@ant-design/icons';
 import axios from 'axios';
 import 'react-diff-view/style/index.css';
 import './styles.css';
@@ -29,7 +30,10 @@ const Appdiff = ({ diffText }) => {
         className="diff-header"
       >
         {oldPath === newPath ? oldPath : `${oldPath} -> ${newPath}`}
-        <Checkbox onChange={handleCollapse}>Collapse</Checkbox>
+        <div>
+          <Checkbox>Ignore</Checkbox>
+          <Checkbox onChange={handleCollapse}>Collapse</Checkbox>
+        </div>
       </header>
       {collapse ? null : (
         <Diff viewType="unified" diffType={type} hunks={hunks}>
@@ -57,12 +61,41 @@ const CodeDiff = () => {
       );
       // console.log(codeRes);
       // console.log('data', codeRes.data.code_diff_list[0]);
-      setCodeDiff(codeRes.data.code_diff_list);
+      await setCodeDiff(codeRes.data.code_diff_list);
     };
     getData();
   }, []);
 
-  console.log(codeDiff[0].diff);
+  const data = [
+    'README.md',
+    'scripts/hulk.js',
+    'src/diff2html.js',
+    'src/joganjs-utils.js',
+    'src/line-by-line-printer.js',
+    'test/side-by-side.js',
+  ];
+
+  const fileChanges = () => {
+    return (
+      <div style={{ marginTop: '20px', marginBottom: '20px' }}>
+        <div style={{ display: 'flex' }}>
+          <h2 style={{ marginRight: '10px' }}>File changed</h2>
+          <Button>Show</Button>
+        </div>
+        <List
+          size="small"
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item>
+              <FileExcelOutlined /> {item}
+            </List.Item>
+          )}
+        />
+      </div>
+    );
+  };
+
+  // console.log(codeDiff[0].diff);
 
   const hey =
     'diff --git a/requirements.txt b/requirements.txt\nindex c3f75dc..2cda10e 100644\n--- a/requirements.txt\n+++ b/requirements.txt\n';
@@ -71,12 +104,13 @@ const CodeDiff = () => {
     <Appdiff diffText={hey + code.diff} />
   ));
 
-  const wow = hey + codeDiff[0].diff;
+  // const wow = hey + codeDiff[0].diff;
 
   return (
     <div style={{ marginTop: '10px', fontFamily: 'Ubuntu Mono' }}>
       {/* <Appdiff diffText={wow} /> */}
-      {mapDiff}
+      {fileChanges()}
+      {codeDiff ? mapDiff : null}
     </div>
   );
 };
