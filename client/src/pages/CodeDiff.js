0154c1bd2995a3a15e8199f46f86f31f5d136084
Thomas Min <thomas_min@sfu.ca>
2021-03-23 21:02:49 -0700

    String literal needs fixing

--- client/src/pages/CodeDiff.js
+++ client/src/pages/CodeDiff.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { parseDiff, Diff, Hunk, Decoration } from 'react-diff-view';
+import axios from 'axios';
 import 'react-diff-view/style/index.css';
 import './styles.css';
 
@@ -33,9 +34,21 @@ const Appdiff = ({ diffText }) => {
   return <div className="ubuntu">{files.map(renderFile)}</div>;
 };
 
-class CodeDiff extends Component {
-  render() {
-    const data = String.raw`diff --git a/requirements.txt b/requirements.txt
+const CodeDiff = () => {
+  const [codeDiff, setCodeDiff] = useState('');
+  useEffect(() => {
+    const getData = async () => {
+      const codeRes = await axios.get(
+        'http://localhost:5678/projects/2/code_diff/0'
+      );
+      console.log('data', codeRes.data.code_diff_list[0]);
+      setCodeDiff(codeRes.data.code_diff_list[0].diff);
+    };
+    getData();
+  }, []);
+
+  console.log(codeDiff);
+  const data = String.raw`diff --git a/requirements.txt b/requirements.txt
 index c3f75dc..2cda10e 100644
 --- a/requirements.txt
 +++ b/requirements.txt
@@ -62,12 +75,17 @@ index afc32ae..9e430e1 100755
 
  test -d "$VENV" || virtualenv -q "$VENV"
 `;
-    return (
-      <div style={{ marginTop: '10px', fontFamily: 'Ubuntu Mono' }}>
-        <Appdiff diffText={data} />
-      </div>
-    );
-  }
-}
+
+  const hey =
+    'diff --git a/requirements.txt b/requirements.txt\nindex 100644\n';
+  const data2 = hey + codeDiff;
+  console.log('data2', data2);
+
+  return (
+    <div style={{ marginTop: '10px', fontFamily: 'Ubuntu Mono' }}>
+      <Appdiff diffText={data2} />
+    </div>
+  );
+};
 
 export default CodeDiff;
