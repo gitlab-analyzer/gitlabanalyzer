0364d9ed7617602907288fd4000dfc6b6e17c115
jlguo <jlguo@sfu.ca>
2021-03-28 16:59:57 -0700

    added typing for code_diff_list

--- server/manager/code_diff_manager.py
+++ server/manager/code_diff_manager.py
@@ -1,6 +1,8 @@
+from typing import List
+
 class CodeDiffManager:
     def __init__(self):
-        self.__code_diff_list = []
+        self.__code_diff_list: List[dict] = []
         self.__num_code_diff = 0
 
     def append_code_diff(self, myCodeDiff: list) -> int:
