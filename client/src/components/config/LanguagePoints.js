8f43eda02198ba40053da024bdc18900c1c265bf
jaddiet <jaddie_tan@sfu.ca>
2021-04-05 04:37:36 -0700

    Change language points to be dynamic

--- client/src/components/config/LanguagePoints.js
+++ client/src/components/config/LanguagePoints.js
@@ -1,86 +1,81 @@
 import React from 'react';
-import { Form, InputNumber, Col, Row } from 'antd';
+import { Form, InputNumber, Col, Row, Button, Input, Space } from 'antd';
 import { configSettings } from '../login/Repo';
+import { CloseOutlined, PlusOutlined } from '@ant-design/icons';
 
 function LanguagePoints() {
   return (
     <div>
-      <h6>Language Points</h6>
-      {/* <Form
-        // label="Languages"
-        name="languages"
-      >             */}
-        <Row gutter={48} >
-          <Col >
-            <Form.Item
-              label="JavaScript"
-              name="javascript"
-              initialValue={(configSettings.javascript && configSettings.javascript) || 1}
-              rules={[
-                { 
-                  required: true, 
-                  message: 'Please input a number.', 
-                },
-              ]}
-            >
-              <InputNumber 
-                  // defaultValue={(configSettings.javascript && configSettings.javascript) || 1}
-                  onChange={value => configSettings.javascript = value} 
-              />
-            </Form.Item>
-            <Form.Item
-              label="Python"
-              name="python"
-              initialValue={(configSettings.python && configSettings.python) || 1}
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input a number.',
-                },
-              ]}
-            >
-              <InputNumber 
-                // defaultValue={(configSettings.python && configSettings.python) || 1}
-                onChange={value => configSettings.python = value} 
-              />
-            </Form.Item>
-          </Col>    
-          <Col >
-            <Form.Item
-              label="HTML"
-              name="html"
-              initialValue={(configSettings.html && configSettings.html) || 1}
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input a number.',
-                },
-              ]}
-            >
-              <InputNumber 
-                // defaultValue={(configSettings.html && configSettings.html) || 1}
-                onChange={value => configSettings.html = value} 
-              />
-            </Form.Item>
-            <Form.Item
-              label="CSS"
-              name="css"
-              initialValue={(configSettings.css && configSettings.css) || 1}
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input a number.',
-                },
-              ]}
-            >
-              <InputNumber 
-                // defaultValue={(configSettings.css && configSettings.css) || 1}
-                onChange={value => configSettings.css = value} 
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-      {/* </Form> */}
+      <h6 
+        style={{paddingBottom:10}}
+      >
+        Language Points
+      </h6>
+      <Row>
+        <Col>
+          <Form.List name="language">
+            {(fields, {add, remove}) => (
+              <div style={{width:300}}>
+                {fields.map((field, index) => (
+                  <Space 
+                    key={field.key} 
+                    style={{ 
+                      display:'flex', 
+                      marginBottom:8 
+                    }}
+                    align="baseline"
+                  >
+                    {/* <p>.</p> */}
+                    <Form.Item
+                      {...field}
+                      name={[field.name, 'extname']}
+                      fieldKey={[field.fieldKey, 'extname']}
+                      // onValuesChange={(field) => {tempitername=field.target.value}}
+                      rules={[{ 
+                        required:true, 
+                        message: 'Missing Extension'
+                      }]}
+                    >
+                      <Input 
+                        placeholder="ex: js" 
+                        // addonBefore="."
+                        // onValuesChange={(field) => {tempitername=field.target.value}}
+                      />
+                      {/* {console.log('tempitername',tempitername, index)} */}
+                    </Form.Item>
+                    <p>:</p>
+                    <Form.Item
+                      {...field}
+                      name={[field.name, 'extpoint']}
+                      fieldKey={[field.fieldKey, 'extpoint']}
+                      rules={[{ 
+                        required: true, 
+                        message: 'Missing Points'
+                      }]}
+                    >
+                      <InputNumber />
+                    </Form.Item>
+                    <CloseOutlined 
+                      style={{ color:'red' }}
+                      onClick={() => remove(field.name)}
+                    />
+                  </Space>
+                ))}
+                <Form.Item>
+                  <Button 
+                    type="dashed"
+                    onClick={() => add()}
+                    icon={<PlusOutlined />}
+                    block
+                  >
+                    Add Language
+                  </Button>
+                </Form.Item>
+              </div>
+            )}
+          </Form.List>
+        </Col>
+      </Row>
     </div>
   );
 }
