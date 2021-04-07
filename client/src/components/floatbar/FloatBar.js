97d34b6150a02c31a974b1a4331964e2a727fe8a
Thomas Min <thomas_min@sfu.ca>
2021-04-06 20:04:48 -0700

    Fix merge conflict from merging master

--- client/src/components/floatbar/FloatBar.js
+++ client/src/components/floatbar/FloatBar.js
@@ -1,18 +1,18 @@
 import React, { useEffect } from 'react';
 import { Select, Button, DatePicker, notification } from 'antd';
-import { CheckCircleOutlined, CopyOutlined, ScanOutlined } from '@ant-design/icons';
+import {
+  CheckCircleOutlined,
+  CopyOutlined,
+  ScanOutlined,
+} from '@ant-design/icons';
 import { useAuth } from '../../context/AuthContext';
-<<<<<<< HEAD
-import EveryoneScore, { ScoreCalculator, barData } from './EveryoneScore.js';
-import { renderToStaticMarkup } from 'react-dom/server';
+import EveryoneScore, {
+  ScoreCalculator,
+  barData,
+  FillBarData,
+} from './EveryoneScore.js';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Grid from '@material-ui/core/Grid';
-import moment from 'moment';
-=======
-import EveryoneScore, { ScoreCalculator, barData, FillBarData } from './EveryoneScore.js';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import Grid from '@material-ui/core/Grid';
->>>>>>> master
 
 import './FloatBar.css';
 
@@ -28,8 +28,8 @@ const copySuccessful = () => {
 };
 
 function FloatBar() {
-  const [ sortType, setSortType ] = React.useState('alpha');
-  
+  const [sortType, setSortType] = React.useState('alpha');
+
   const {
     membersList,
     usersList,
@@ -44,33 +44,39 @@ function FloatBar() {
   } = useAuth();
   useEffect(() => {}, []);
 
-  let dateObj = {}
+  let dateObj = {};
   if (currentConfig && currentConfig.iterations) {
-    for(let dateprop of currentConfig.iterations){
-      dateObj[dateprop.itername] = [dateprop.iterdates[0], dateprop.iterdates[1]]
+    for (let dateprop of currentConfig.iterations) {
+      dateObj[dateprop.itername] = [
+        dateprop.iterdates[0],
+        dateprop.iterdates[1],
+      ];
     }
   }
-
 
   const handleSort = (value) => {
-    setSortType(value)
-    if (value === "alpha"){
-      barData.sort((a,b) => (a.name > b.name) ? 1 : -1)
+    setSortType(value);
+    if (value === 'alpha') {
+      barData.sort((a, b) => (a.name > b.name ? 1 : -1));
+    } else if (value === 'low') {
+      barData.sort((a, b) =>
+        ScoreCalculator(a.name) > ScoreCalculator(b.name) ? 1 : -1
+      );
+    } else if (value === 'high') {
+      barData.sort((a, b) =>
+        ScoreCalculator(a.name) < ScoreCalculator(b.name) ? 1 : -1
+      );
     }
-    else if (value === "low"){
-      barData.sort((a,b) => (ScoreCalculator(a.name) > ScoreCalculator(b.name)) ? 1 : -1)
-    }
-    else if (value === "high"){
-      barData.sort((a,b) => (ScoreCalculator(a.name) < ScoreCalculator(b.name)) ? 1 : -1)     
-    }
-  }
+  };
 
-  let userData = barData.find(x=>x.name===selectUser)
-
+  let userData = barData.find((x) => x.name === selectUser);
 
   return (
     <listitem>
-      <div className="floatbar-header" style={{height:10, backgroundColor:'white'}} />
+      <div
+        className="floatbar-header"
+        style={{ height: 10, backgroundColor: 'white' }}
+      />
       <div className="floatbar-container">
         <div className="floatbaralign">
           <EveryoneScore />
@@ -85,7 +91,7 @@ function FloatBar() {
           >
             <Grid item xs={12}>
               <div className="daterange">
-                <RangePicker 
+                <RangePicker
                   defaultValue={dataList}
                   onChange={setDataList}
                   format="YYYY/MM/DD hh:mm:ss"
@@ -109,27 +115,25 @@ function FloatBar() {
               </div>
             </Grid>
             <Grid item xs={12}>
-                <CopyToClipboard
-                  format={'text/plain'}
-                  text={ (userData &&
-                    (
-                      String(ScoreCalculator(userData.name).toFixed(0)) + '\t' +
-                      userData.commits + '\t' +
-                      userData.code + '\t' +
-                      userData.issue
-                    )) || (
-                      '0\t0\t0\t0'
-                    )
-                  }
-                >
-                  <Button 
-                    style={{ width: 150 }} 
-                    onClick={copySuccessful}
-                  >
-                    Copy
-                    <CopyOutlined className="copyicon" />
-                  </Button>
-                </CopyToClipboard>
+              <CopyToClipboard
+                format={'text/plain'}
+                text={
+                  (userData &&
+                    String(ScoreCalculator(userData.name).toFixed(0)) +
+                      '\t' +
+                      userData.commits +
+                      '\t' +
+                      userData.code +
+                      '\t' +
+                      userData.issue) ||
+                  '0\t0\t0\t0'
+                }
+              >
+                <Button style={{ width: 150 }} onClick={copySuccessful}>
+                  Copy
+                  <CopyOutlined className="copyicon" />
+                </Button>
+              </CopyToClipboard>
               {/* )} */}
             </Grid>
           </Grid>
