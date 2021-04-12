41368a2b53b0c8286c37b60eb59a954a85a3a79d
fanghaof <henry_fang@sfu.ca>
2021-04-11 23:18:19 -0700

    Update gitlab_code_diff_test.py

--- server/test/gitlab_code_diff_test.py
+++ server/test/gitlab_code_diff_test.py
@@ -1,8 +1,9 @@
 import os
 import sys
 sys.path.append(os.path.abspath("./server"))
-from interface.gitlab_interface import GitLab
 from interface.gitlab_project_interface import GitLabProject
+from interface.gitlab_interface import GitLab
+from model.project import Project
 
 """
 Below are only for testing purpose
@@ -11,7 +12,6 @@ if __name__ == "__main__":
     myGitLab = GitLab(token="EJf7qdRqxdKWu1ydozLe", url="https://cmpt373-1211-12.cmpt.sfu.ca/")
     if myGitLab.authenticate():
         print("authenticated")
-    project = myGitLab.find_project(2)
-    print("Updating all managers...")
-    gitlabProjectInterface = GitLabProject(project)
+    gitLabProject = GitLabProject(project=myGitLab.find_project(2))
+    gitLabProject.update(myGitLab)
     print("Done!")
