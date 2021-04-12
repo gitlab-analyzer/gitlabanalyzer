0161199be404c63e840342ff81b3f24f5cfd0d69
Henry Fang <fanghaof@sfu.ca>
2021-04-12 00:21:00 -0700

    Update gitlab_code_diff_test.py

--- server/test/gitlab_code_diff_test.py
+++ server/test/gitlab_code_diff_test.py
@@ -4,6 +4,8 @@ sys.path.append(os.path.abspath("./server"))
 from interface.gitlab_project_interface import GitLabProject
 from interface.gitlab_interface import GitLab
 from model.project import Project
+from model.code_diff import CodeDiff
+from manager.code_diff_Analyzer import CodeDiffAnalyzer
 
 """
 Below are only for testing purpose
@@ -12,6 +14,11 @@ if __name__ == "__main__":
     myGitLab = GitLab(token="EJf7qdRqxdKWu1ydozLe", url="https://cmpt373-1211-12.cmpt.sfu.ca/")
     if myGitLab.authenticate():
         print("authenticated")
-    gitLabProject = GitLabProject(project=myGitLab.find_project(2))
+    project = myGitLab.find_project(2)
+    gitLabProject = GitLabProject(project=project)
     gitLabProject.update(myGitLab)
     print("Done!")
+    temp = CodeDiffAnalyzer()
+    diffs = project.commits.get("d95a38e6").diff()
+    for diff in diffs:
+        print(temp.get_code_diff_statistic(CodeDiff(diff)))
